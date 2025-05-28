f38ca74e8deab17a51294f13651ba9e890c68ea4
Isaiah <zahpotts123@gmail.com>
2025-05-28 18:39:22 -0400

    added changes for render deployment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Specify your client's origin
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
